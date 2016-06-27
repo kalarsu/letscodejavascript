6fadf23052b55169acab9a688c7693a15f4d917d
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-06-27 11:20:53 -0700

    build script checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,21 @@
 (function(){
     "use strict";//help javascript prevent errors, not allow sloppy coding
 
+    var EXPECTED_NODE_VERSION = "v5.5.0";
+
     desc("default build");//documentation for following task, >jake --tasks or >jake -T  will show all the tasks, this is what it meant self-documentation
-    task("default", function(){
+    task("default", ["version"], function(){ //run "version" task before running default
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version");
+    task("version", function(){
+        console.log("Checking Node version: .");
+
+        let actualVersion = process.version; //current node version
+        if(actualVersion !== EXPECTED_NODE_VERSION){
+            fail("Incorrect Node version: expedted:" + EXPECTED_NODE_VERSION + ", but was: "+actualVersion);  //fail is a Jake function
+        }
     });
 
 }());
