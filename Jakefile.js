50bd2042354a748ecfa67c51771f10390d16c97f
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-06-28 10:34:32 -0700

    User Semver to compare Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,8 @@
 (function(){
     "use strict";//help javascript prevent errors, not allow sloppy coding
 
+    var semver = require("semver");
+
     desc("default build");//documentation for following task, >jake --tasks or >jake -T  will show all the tasks, this is what it meant self-documentation
     task("default", ["version"], function(){ //run "version" task before running default
         console.log("\n\nBUILD OK");
@@ -10,14 +12,12 @@
     task("version", function(){
         console.log("Checking Node version: .");
 
-
         var packageJson = require("./package.json"); //require was build into node
-        var expectedVersion = "v" + packageJson.engines.node;
+        var expectedVersion = packageJson.engines.node;
 
         let actualVersion = process.version; //current node version
-        if(actualVersion !== expectedVersion){
+        if( semver.neq(actualVersion, expectedVersion) ){//neq : not equal
             fail("Incorrect Node version: expedted:" + expectedVersion + ", but was: "+actualVersion);  //fail is a Jake function
         }
     });
-
 }());
