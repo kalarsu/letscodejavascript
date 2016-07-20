38e090f4fc223f6bc8f5d4a0d2dee56f9169d24f
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-07-20 11:15:58 -0700

    adding Mocha test in test.js

--- src/test.js
+++ src/test.js
@@ -6,15 +6,20 @@
 
     var assert = require("chai").assert;
 
-    //basic addition
-    assert.equal(add(3,4), 7); //simeple one line chai replace following write your own assertEqual function
+    //Mocha--------------------------------------
+    describe("Addition", function(){  //use "describe" to group all the test cases
+        it("adds positive numbers", function(){ //use "it" for test case, and write the comment in the code
+            assert.equal(add(3,4), 7); //Chai
+        });
 
-    //IEEE 754 floating point
-    assert.equal(add(0.1, 0.2), 0.30000000000000004);
+        it("uses IEEE 754 floating point", function(){
+            assert.equal(add(0.1, 0.2), 0.30000000000000004);//Chai
+        });
+    });
 
 
     function add(a,b){
-        return a+b;
+        return a + b;
     }
 
 }());
