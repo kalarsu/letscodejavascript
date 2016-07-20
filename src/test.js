ea9497f3a64d21c1c5f3aa9d01157ce37f11cc15
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-07-20 10:06:50 -0700

    Added floating point test

--- src/test.js
+++ src/test.js
@@ -6,15 +6,15 @@
 
     var assert = require("chai").assert;
 
+    //basic addition
     assert.equal(add(3,4), 7); //simeple one line chai replace following write your own assertEqual function
 
-    //assertEqual(add(3,4), 7);
+    //IEEE 754 floating point
+    assert.equal(add(0.1, 0.2), 0.30000000000000004);
+
 
     function add(a,b){
         return a+b;
     }
 
-    // function assertEqual(actual, expected){
-    //     if(actual !== expected) throw new Error("Expected=" + expected + ", but got="+actual);
-    // }
 }());
