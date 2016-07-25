2202ff0a9650b5cf6ced517d334bd8baa9b183f0
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-07-25 16:26:57 -0700

    Factored out example test into multipile modules

--- src/_addition_test.js
+++ src/_addition_test.js
@@ -4,28 +4,25 @@
 (function () {
     "use strict";
 
+    var addition = require("./addition.js");
+
     //var assert = require("chai").assert;
 
     //Mocha--------------------------------------
     describe("Addition", function(){  //use "describe" to group all the test cases
         it("adds positive numbers", function(){ //use "it" for test case, and write the comment in the code
             //assert.equal(add(3,4), 7); //Chai
-            assertEqual(add(3,4), 7);
+            assertEqual(addition.add(3,4), 7);
         });
 
         it("uses IEEE 754 floating point", function(){
             //assert.equal(add(0.1, 0.2), 0.30000000000000004);//Chai
-            assertEqual(add(0.1,0.2), 0.30000000000000004);
+            assertEqual(addition.add(0.1,0.2), 0.30000000000000004);
         });
 
         function assertEqual(actual, expected){
             if (actual !== expected) throw new Error("expected"+ expected + ", but was " + actual);
         }
     });
-
-
-    function add(a,b){
-        return a + b;
-    }
 
 }());
---
+++ src/addition.js
@@ -0,0 +1,13 @@
+/**
+ * Created by c.su on 7/18/16.
+ */
+(function () {
+    "use strict";
+    
+    
+    
+    exports.add = function add(a,b){
+        return a + b;
+    };
+
+}());
