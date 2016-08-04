891fdbc37dbbb7b53a3ce2c2755d89afd3ba0f8a
kalarsu <root@sfo-csu02-mac.corp.kabam.com>
2016-08-03 18:13:24 -0700

    testing javascript add modify and delete DOM and test in DEBUG mode

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -10,7 +10,15 @@
 
     describe("Something", function(){
         it("Something", function(){
+            var div = document.createElement("div");
+            div.innerHTML = "This is an example";
+            document.body.appendChild(div);
 
+            var p = document.createElement("p");
+            p.innerHTML = "A new paragraph";
+            div.appendChild(p);
+
+            div.parentNode.removeChild(div);
         });
     });
 
